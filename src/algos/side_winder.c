cc4c5736b91f6c833ce6c19eec7d96b28a10d2f3
Jamesbarford <jamesbarfordevans@gmail.com>
2021-02-03 19:56:11 +0000

    working sidewinder

--- src/algos/side_winder.c
+++ src/algos/side_winder.c
@@ -5,36 +5,30 @@ void side_winder_link(Grid *g)
 	for (unsigned int row = 0; row < g->rows; ++row)
 	{
 		Cell *run[g->columns];
+		Cell *cell;
 		int ptr = 0;
 		for (unsigned int column = 0; column < g->columns; ++column)
 		{
-			Cell cell = g->board[row][column];
+			cell = &g->board[row][column];
+			run[ptr++] = cell;
 
-			run[ptr++] = &cell;
-
-			int random = rand() % 2;
-			bool at_east = cell.coords->east == NULL;
-			bool at_north = cell.coords->north == NULL;
-			bool close_run = at_east || (!at_north && random == 0);
+			bool at_east = cell->coords->east == NULL;
+			bool at_north = cell->coords->north == NULL;
+			bool close_run = at_east || (!at_north && (rand() % 2) == 0);
 
 			if (close_run)
 			{
 				int idx = rand() % ptr;
-				Cell link = *run[idx];
-				if (link.coords->north != NULL)
-				{
-					add_cell_link(&link, NORTH, link.coords->north, true);
-				}
-				for (int i = 0; i < column; ++i)
-				{
+				Cell *link = run[idx];
+				if (link && link->coords->north != NULL)
+					add_cell_link(link, NORTH, link->coords->north, true);
+				// clear run;
+				for (int i = 0; i < g->columns; ++i)
 					run[i] = NULL;
-					ptr = 0;
-				}
+				ptr = 0;
 			}
 			else
-			{
-				add_cell_link(&cell, EAST, cell.coords->east, true);
-			}
+				add_cell_link(cell, EAST, cell->coords->east, true);
 		}
 	}
 }
